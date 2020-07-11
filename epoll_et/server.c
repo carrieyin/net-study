ada0120bb65f92863fa844e277db1c608e108167
carrieyin <695685009@qq.com>
2020-07-11 21:38:46 +0800

    epoll et模式

--- epoll_et/server.c
+++ epoll_et/server.c
@@ -1,4 +1,5 @@
 #include "../common/wrapper.h"
+
 //mac下无epoll
 #include <sys/epoll.h>
 
@@ -35,20 +36,32 @@ int main()
         }
 
        struct epoll_event ep[OPEN_MAX];
-       int nready = epoll_wait(efd, ep, OPEN_MAX, -1);
-       if(nready > 0)
-       {
-           char buf[BUFSIZ];
-           int nread = Read(pfd[0], buf, BUFSIZ);
-           printf("read %d \n", nread);
-           write(STDOUT_FILENO, buf, nread);
-
+       while (1)
+       { 
+            int nready = epoll_wait(efd, ep, OPEN_MAX, -1);
+            printf("ready %d \n", nready);
+            if(nready > 0)
+            {
+                char buf[BUFSIZ];
+                int nread = Read(pfd[0], buf, BUFSIZ);
+                printf("read %d \n", nread);
+                write(STDOUT_FILENO, buf, nread);
+            }
        }
+          
    }
    else //父进程
    {
+
        close(pfd[0]);
-       write(pfd[0], "aaa\n", 5);
+       while (1)
+       {
+           int n = write(pfd[1], "aaa\n", 5);
+           printf("write %d \n",n);
+           sleep(3);
+       }
+       
+       
    }
    
 } 
