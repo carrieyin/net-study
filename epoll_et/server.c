b70aa2c5a266320730a38fb062ab4d0724db5fd2
carrieyin <695685009@qq.com>
2020-07-11 22:06:11 +0800

    epoll et lt测试

--- epoll_et/server.c
+++ epoll_et/server.c
@@ -1,6 +1,6 @@
 #include "../common/wrapper.h"
 
-//mac下无epoll
+//mac下无epoll，仅演示epoll可以用于监听普通文件描述符，实际读写pipe不需要
 #include <sys/epoll.h>
 
 #define PORT 8888 
@@ -23,11 +23,12 @@ int main()
        int efd = epoll_create(OPEN_MAX);
         if(efd == -1)
         {
-        sys_err("epoll error");
+            sys_err("epoll error");
         }
 
         struct epoll_event tep;
         tep.data.fd = pfd[0];
+        //tep.events = EPOLLIN | EPOLLET;
         tep.events = EPOLLIN;
         ssize_t res = epoll_ctl(efd, EPOLL_CTL_ADD, pfd[0], &tep);
         if(res == -1)
@@ -39,16 +40,15 @@ int main()
        while (1)
        { 
             int nready = epoll_wait(efd, ep, OPEN_MAX, -1);
-            printf("ready %d \n", nready);
+            //printf("ready %d \n", nready);
             if(nready > 0)
             {
                 char buf[BUFSIZ];
-                int nread = Read(pfd[0], buf, BUFSIZ);
-                printf("read %d \n", nread);
+                int nread = Read(pfd[0], buf, 4);
+                //printf("read %d \n", nread);
                 write(STDOUT_FILENO, buf, nread);
             }
-       }
-          
+       }        
    }
    else //父进程
    {
@@ -56,12 +56,10 @@ int main()
        close(pfd[0]);
        while (1)
        {
-           int n = write(pfd[1], "aaa\n", 5);
-           printf("write %d \n",n);
+           int n = write(pfd[1], "aaaa\nbbbb\n", 10);
+           //printf("write %d \n",n);
            sleep(3);
        }
-       
-       
    }
    
 } 
