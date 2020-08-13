f3507590e400cac256ec5c51bc9519e3db09c45e
carrieyin <carrieyin@users.noreply.github.com>
2020-08-13 03:04:27 +0000

    Delete sync.sh
    删除

--- sync.sh
+++
@@ -1,6 +0,0 @@
-while [ "true" ]; do
-    echo '----------------------------------------------------------------------------'
-    fswatch -r -L -1 *
-    date
-    rsync -r * rdtfare@10.221.155.87:/home/rdtfare/rdydd/ep
-done
